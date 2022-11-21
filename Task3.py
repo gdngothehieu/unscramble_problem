89aa5bc1a846e10bb5341eec692f0462350122b5
tchong6@slb.com <ngothehieusan123@gmail.com>
2022-11-21 08:01:23 +0700

    abc

--- Task3.py
+++ Task3.py
@@ -17,9 +17,9 @@ TASK 3:
 (080) is the area code for fixed line telephones in Bangalore.
 Fixed line numbers include parentheses, so Bangalore numbers
 have the form (080)xxxxxxx.)
-
 Part A: Find all of the area codes and mobile prefixes called by people
-in Bangalore.
+in Bangalore. In other words, the calls were initiated by "(080)" area code
+to the following area codes and mobile prefixes:
  - Fixed lines start with an area code enclosed in brackets. The area
    codes vary in length but always begin with 0.
  - Mobile numbers have no parentheses, but have a space in the middle
@@ -27,57 +27,56 @@ in Bangalore.
    is its first four digits, and they always start with 7, 8 or 9.
  - Telemarketers' numbers have no parentheses or space, but they start
    with the area code 140.
-
 Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
-
 Part B: What percentage of calls from fixed lines in Bangalore are made
 to fixed lines also in Bangalore? In other words, of all the calls made
 from a number starting with "(080)", what percentage of these calls
 were made to a number also starting with "(080)"?
-
 Print the answer as a part of a message::
 "<percentage> percent of calls from fixed lines in Bangalore are calls
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
-# Create a list to hold all the numbers called by people in Bangalore
-#  that starts with (0...)
-all_nums_called = []
-# Create a set to hold the area codes
-unique_codes = set()
-i = 0
 
-# While loop to loop over the doc, until the end of it
-# Find the numbers that starts with (080) in the first column
-# Then append to the all_codes list the number called by it (second column)
-while i < len(calls):
-    if '(080)' in calls[i][0]:
-        all_nums_called.append(calls[i][1])
-        # If the number start with a code '(0', then split to get just the code,
-        # add to the end of the code ')', once we lost it during split
-        if '(0' in calls[i][1]:
-            code = (calls[i][1].split(')')[0]) + ')'
-            # Once unique_codes is a set, it will add the code only when it is not there already
-            unique_codes.add(code)
-    i += 1
 
-print("The numbers called by people in Bangalore have codes:"
-      + '\n' + '\n'.join(sorted(unique_codes)))
+def is_fixed_Bangalore(number):
+    return number[:5] == '(080)'
 
-# To find how many times a person from Bangalore calls some one in Bangalore:
-# For each number in all_nums_called check if has '(080)' (Bangalore)
-# If does adds 1 to count
-count = 0
-for num in all_nums_called:
-    if '(080)' in num:
-        count += 1
 
-# To find the percentage, get the count variable, and divide by the len of all_nums_called
-# Use round to get just the two first decimals
-percent = round((count * 100)/len(all_nums_called), 2)
+def is_fixed(number):
+    return number[:2] == '(0'
 
-print(f"{percent} percent of calls from fixed lines in Bangalore are calls "
-      "to other fixed lines in Bangalore.")
+
+def is_mobile(number):
+    return number[0] == '7' or number[0] == '8' or number[0] == '9' and ' ' in number
+
+
+def is_tele(number):
+    return number[:3] == '140'
+
+
+set_of_codes = set()
+calls_from_fixed_Bangal = 0
+calls_counted = 0
+for call in calls:
+    if is_fixed_Bangalore(call[0]):  # From Bangalor
+        calls_from_fixed_Bangal += 1
+        if is_fixed(call[1]):
+            last_index = call[1].find(')')
+            set_of_codes.add(call[1][1:last_index])
+        if is_mobile(call[1]):
+            set_of_codes.add(call[1][:4])
+        if is_tele(call[1]):
+            set_of_codes.add('140')
+        if is_fixed_Bangalore(call[1]):
+            calls_counted += 1
+
+set_of_codes = sorted(set_of_codes)
+print("The numbers called by people in Bangalore have codes:")
+for code in set_of_codes:
+    print(code)
+percentage = round(calls_counted / calls_from_fixed_Bangal * 100, 2)
+print(F"{percentage} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore")
